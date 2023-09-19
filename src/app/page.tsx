a7d1ac9a3d17a7e0a8134e99c8933b1464a2590e
Krishna <kanavkrishna05@gmail.com>
2023-09-19 23:41:53 +0530

    main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import bg from '@/assets/bg.jpg';
 import logoLeft from '@/assets/MainWeb.png';
 import logoRight from '@/assets/Learn.png';
 import constitution from '@/assets/preamble.jpg';
 import topLogo from '@/assets/Logo.png';
-import Image from 'next/image';
-import Link from 'next/link';
-import bg from '@/assets/bg.jpg';
 
 const CenterImagePage = () => {
   return (
     <div className="relative flex flex-col items-center justify-center">
-      {/* <div
-        className="absolute top-0 left-0 w-full h-full bg-cover opacity-20"
-        style={{ backgroundImage: 'url(@/assests/bg.jpg)' }}
-      /> */}
-      <Image
-      src={bg}
-      alt="img"
-      className="absolute top-0 left-0 w-full h-full bg-cover opacity-40"
-      />
-
+      <Image src={bg} alt="img" className="absolute top-0 left-0 w-full h-full bg-cover opacity-40" />
+      
       <div className="flex items-center justify-center w-full mb-5 relative z-10">
         <Image
           src={topLogo}
@@ -29,12 +21,18 @@ const CenterImagePage = () => {
           className="mt-7"
         />
       </div>
+      
       <div className="flex gap-20 justify-center">
         <div>
-          <div className="border rounded-full mr-8 mt-44">
-            <Image src={logoLeft} alt="logo" className="mr-10" height={180} />
+          <div className="border rounded-full mr-8 mt-44 transition-transform duration-300 transform hover:scale-110">
+            <Link href="/home-page" legacyBehavior>
+              <a>
+                <Image src={logoLeft} alt="logo" className="mr-10" height={180} />
+              </a>
+            </Link>
           </div>
         </div>
+        
         <Image
           src={constitution}
           alt="constitution"
@@ -42,9 +40,14 @@ const CenterImagePage = () => {
           width={400}
           height={1200}
         />
+        
         <div>
-          <div className="border rounded-full ml-10 mt-44">
+          <div className="border rounded-full ml-10 mt-44 transition-transform duration-300 transform hover:scale-110">
+          <Link href="/learning" legacyBehavior>
+              <a>
             <Image src={logoRight} alt="logo" className="ml-10" height={190} />
+              </a>
+          </Link>
           </div>
         </div>
       </div>
